472919aa7afc95f74bdfb1046ec78802a7d2525d
Josh Arbon <jarbon@student.neumont.edu>
2025-06-09 11:19:13 -0600

    oops

--- algos/awesome.js
+++
@@ -1,6 +0,0 @@
-let numbers = [1, 2, 3, 4, 5, "number"]
-for (number in numbers) {
-    console.log("this is definitely a number: ", number)
-}
-
-console.log("Still a number: ", numbers[5])
--- algos/preparingForTheJourney/algorithms.py
+++
@@ -1,8 +0,0 @@
-class Algorithm():
-    def bubble_ascending(self, array):
-        # implement bubble sort
-        pass
-    
-    def swap(self, array, idx1, idx2):
-        array[idx1], array[idx2] = array[idx2], array[idx1]
-        return array
